c5fa7b62aa151be2f85867bbc55076c80869ef0f
pottsaf <aaronfpotts@gmail.com>
2023-04-26 18:55:54 -0600

    Closure on main function, forEach to display each book already in array

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # library
 
-- _ Book object
+- [x] Book object
 	- Title
 	- Author
 	- Number of pages
 	- Read or not
 	- Memorable quote
-- %cross; Library array of objects
+- [x] Library array of objects
 - [x] Use closure
 	- [ ] Refactor adding data to accept only certain inputs
-- [ ] Display array of books and update whenever a book is added
+- [x] Display array of books
+- [ ] Be able to add book
+- [ ] Update page whenever a book is added
 - [ ] Output as flexbox items
 	- [ ] Sort at random on every window load.
 
--- scripts.js
+++ scripts.js
@@ -1,51 +1,59 @@
-function book (title, author, pages, read = false, quote) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.quote = quote
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages long`
-  }
-};
-
-function haveRead (book) {
-  if (book.read === false) {
-    return 'This book has not been read.'
-  } else {
-    return 'This book has been read.'
-  }
-}
-
 const display = document.getElementById('display')
-
-const theHiddenDimension = new book('The Hidden Dimension', 'Edward T. Hall', 195)
 
 const library = (() => {
   const books = []
+
+  function Book (title, author, pages, read = false, quote) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.quote = quote
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages long`
+    }
+  }
   function list () {
     return books
+  }
+  function haveRead (book) {
+    if (book.read === false) {
+      return 'You haven\'t read this book.'
+    } else {
+      return 'You\'ve read this book.'
+    }
   }
   function info (num) {
     return books[num].info() + '. ' + haveRead(books[num])
   }
-  function addBook (book) {
+  function addBook (title, author, pages, read, quote) {
+    const book = new Book(title, author, pages, read, quote)
     books.unshift(book)
-  }
-  function update () {
-    display.textContent = library.info(0)
   }
   return {
     books: books,
     addBook: addBook,
     list: list,
-    info: info,
-    update: update
-  };
-})();
+    info: info
+  }
+})()
 
-library.addBook(theHiddenDimension)
+function booksCheck() {
+  console.table(library.list())
+}
+
+library.addBook('The Hidden Dimension', 'Edward T. Hall', 195)
+
+library.addBook('Meditations', 'Marcus Aurelius', 191, true)
+
 console.table(library.list())
-display.textContent = library.info(0)
+
+library.books.forEach((book) => {
+  display.innerHTML += `${book.info()}<br>`
+});
+
+/* Use a for loop to load all objects in array on page */
 
 /* Add functions to display array of books and update whenever a book is added */
+
+/* Add an element and add card class to display on page. */
