bf2da43b17598a0496c3c022a7b41934902db26b
pottsaf <aaronfpotts@gmail.com>
2023-04-26 20:07:30 -0600

    Refine display in html

--- scripts.js
+++ scripts.js
@@ -2,32 +2,28 @@ const display = document.getElementById('display')
 
 const library = (() => {
   const books = []
+  const addABook = document.getElementsByTagName('button')
 
-  function Book (title, author, pages, read = false, quote) {
+  function Book (title, author, pages, read = false, quote = 'No quote (yet)', quoteauthor = '') {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.quote = quote
+    this.quoteauthor = quoteauthor
     this.info = function () {
-      return `${this.title} by ${this.author}, ${this.pages} pages long`
+      return `<u>${this.title}</u> by <i>${this.author}</i>, ${this.pages} pages long`
     }
   }
   function list () {
     return books
   }
-  function haveRead (book) {
-    if (book.read === false) {
-      return 'You haven\'t read this book.'
-    } else {
-      return 'You\'ve read this book.'
-    }
-  }
+
   function info (num) {
     return books[num].info() + '. ' + haveRead(books[num])
   }
-  function addBook (title, author, pages, read, quote) {
-    const book = new Book(title, author, pages, read, quote)
+  function addBook (title, author, pages, read, quote, quoteauthor) {
+    const book = new Book(title, author, pages, read, quote, quoteauthor)
     books.unshift(book)
   }
   return {
@@ -38,22 +34,31 @@ const library = (() => {
   }
 })()
 
+function haveRead (book) {
+  if (book.read === false) {
+    return 'This book has not been read.'
+  } else {
+    return 'This book has been read.'
+  }
+}
+
 function booksCheck() {
   console.table(library.list())
 }
 
 library.addBook('The Hidden Dimension', 'Edward T. Hall', 195)
-
-library.addBook('Meditations', 'Marcus Aurelius', 191, true)
+library.addBook('Meditations', 'Marcus Aurelius', 191, true, 'The book is full of quotes.')
+library.addBook('Crooked Kingdom', 'Leigh Bardugo', 546, true, 'You\'re not weak because you can\'t read. You\'re weak because you\'re afraid of people seeing your weakness. You\'re letting shame decide who you are.', 'Kaz')
 
 console.table(library.list())
 
 library.books.forEach((book) => {
-  display.innerHTML += `${book.info()}<br>`
+  display.innerHTML += `<h3>${book.info()}</h3><blockquote><h5>${book.quote}</h5></blockquote><figcaption>${book.quoteauthor}</figcaption><br>${haveRead(book)}`
 });
-
-/* Use a for loop to load all objects in array on page */
 
 /* Add functions to display array of books and update whenever a book is added */
 
 /* Add an element and add card class to display on page. */
+
+/* library.books.findIndex(i => i.title === "Crooked Kingdom")
+This can be used to search. It'll need to be all lower case and be able to search part of the string while typing in the search bar */
